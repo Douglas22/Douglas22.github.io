de33fd70795edbab145856b17431f9e6363a5562
Douglas22 <douglasalderman23@gmail.com>
2017-08-22 03:00:17 +0100

    contact form tempry replacement added till migration to non static hosting

--- js/contact_me.js
+++ js/contact_me.js
@@ -47,10 +47,11 @@ $(function() {
                 },
                 error: function() {
                     // Fail message
+					
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com;>me@example.com</a> ? Sorry for the inconvenience!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " this should be avalible after after I migrate the sight...</strong> for now please email me directly useing the email given above<a href='douglasalderman23@gmail.com?Subject=Message_Me from myprogrammingblog.com;>me@example.com</a> ? Sorry for the inconvenience!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
